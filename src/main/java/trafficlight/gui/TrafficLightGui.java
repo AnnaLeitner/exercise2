993ea45407c579a084fdc70139ec3922d5d70763
AnnaLeitner <71489265+AnnaLeitner@users.noreply.github.com>
2021-06-07 10:01:27 +0200

    color change

--- src/main/java/trafficlight/gui/TrafficLightGui.java
+++ src/main/java/trafficlight/gui/TrafficLightGui.java
@@ -32,9 +32,9 @@ public class TrafficLightGui extends JFrame implements ActionListener {
     private void initLights(TrafficLightCtrl ctrl) {
         //TODO implement a part of the pattern here
         //create the TrafficLight
-        green = new TrafficLight(Color.pink);
-        yellow = new TrafficLight(Color.cyan);
-        red = new TrafficLight(Color.magenta);
+        green = new TrafficLight(Color.green);
+        yellow = new TrafficLight(Color.yellow);
+        red = new TrafficLight(Color.red);
 
         //connect subject and observer
         ctrl.getGreenState().addObserver(green);
